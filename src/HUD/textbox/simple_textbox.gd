9f8aa8814dbc89f3652a56e6495872313535ef40
streq <strequena@gmail.com>
2022-12-07 00:12:06 -0300

    hacer andar el agregado de palabras al textbox

--- src/HUD/textbox/simple_textbox.gd
+++ src/HUD/textbox/simple_textbox.gd
@@ -2,6 +2,7 @@ extends Label
 
 var full_text := ""
 var current_character_index := 0
+var characters_skipped_count := 0
 const whitespace_characters = [" ", "\n"]
 
 func say(request:TextRequest):
@@ -11,11 +12,17 @@ func say(request:TextRequest):
 	while full_text.length() != text.length():
 		while get_line_count()-lines_skipped <= max_lines_visible and full_text.length() != text.length():
 			character_index = show_next_visible_character(full_text, character_index)
-			yield(get_tree().create_timer(0.1),"timeout")
+			yield(get_tree().create_timer(0.5),"timeout")
 		yield(self,"next")
 		lines_skipped += max_lines_visible
 		
-
+func say_new(request:TextRequest):
+	full_text = request.text
+	current_character_index = 0
+	characters_skipped_count = 0
+	text = ""
+	show_next_bit()
+	
 func show_next_visible_character(full_text, character_index):
 	var text_to_add = ""
 	while character_index != full_text.length():
@@ -25,15 +32,25 @@ func show_next_visible_character(full_text, character_index):
 		if !(character in [" ", "\n"]):
 			break
 	text += text_to_add
+	
 	return character_index
 	
 
 func show_next_bit():
-	if next_character_is_whitespace():
-		add_next_word_to_text_and_hide_it()
-	show_next_character()
-	
-	pass
+	while full_text.length() != text.length():
+		var word_length = add_next_word_to_text_and_hide_it()
+		var line_count = get_line_count()
+		if line_count-lines_skipped > max_lines_visible:
+			yield(self,"next")
+			lines_skipped = line_count - (line_count % max_lines_visible)
+			characters_skipped_count = get_total_character_count() - word_length
+		
+		var characters_in_panel_count = get_total_character_count() - characters_skipped_count
+		
+		visible_characters = characters_in_panel_count - word_length
+		while visible_characters != characters_in_panel_count:
+			show_next_character()
+			yield(get_tree().create_timer(0.1),"timeout")
 
 func add_next_word_to_text_and_hide_it():
 	#add all whitespace between current index and next word
@@ -47,24 +64,23 @@ func add_next_word_to_text_and_hide_it():
 	#add all characters from next word
 	var visible_character_count = 0
 	while (
-		current_character_index + visible_character_count != full_text.length() and 
-		!(full_text[current_character_index + visible_character_count] in whitespace_characters)
+		current_character_index + whitespace_count + visible_character_count != full_text.length() and 
+		!(full_text[current_character_index + whitespace_count + visible_character_count] in whitespace_characters)
 	):
 		visible_character_count += 1
-	
-	text += full_text.substr(current_character_index, whitespace_count+visible_character_count)
-	visible_characters = get_total_character_count()-visible_character_count
-	
+	var word = full_text.substr(current_character_index, whitespace_count+visible_character_count)
+	text += word
+	current_character_index = text.length()
+	print("word:", word)
+	print("word_length:", visible_character_count)
+	return visible_character_count
 	pass
 	
 func show_next_character():
-	pass
+	visible_characters+=1
 	
 func next_character_is_whitespace()->bool:
 	return full_text[current_character_index] in whitespace_characters
-
-func _physics_process(delta: float) -> void:
-	pass
 
 
 signal next()
