57c7c2a9028c4f33bc777529a0d35e28a6b8fd2f
streq <strequena@gmail.com>
2022-12-23 10:00:40 -0300

    cambios de paleta, arreglo de stutter de la camara

--- src/test/player/camera.gd
+++ src/test/player/camera.gd
@@ -13,20 +13,20 @@ export var velocity := Vector2()
 func _ready():
 	yield(get_parent(),"ready")
 	offset.y = 0.0
-	offset.x = 40*get_parent().facing_dir
+#	offset.x = 40*get_parent().facing_dir
 
 func _process(delta):
 #	offset = -global_position.posmod(1.0)
 	var parent = get_parent()
 	var h_dir := sign(parent.facing_dir)
 	var v_dir := sign(parent.look_up_dir)
-	
-	
+
+
 	var target_x = h_dir*horizontal_lerp_target if h_dir else sign(offset.x)*(horizontal_extent-horizontal_lerp_target)
 	var target_y = v_dir*vertical_lerp_target if v_dir else sign(offset.y)*(vertical_extent-vertical_lerp_target)
-	
+
 	offset.x = clamp(lerp(offset.x, target_x, delta*lerp_speed), -horizontal_extent, horizontal_extent)
-	offset.y = clamp(lerp(offset.y, target_y, delta*lerp_speed), -vertical_extent, vertical_extent)
+	offset.y = clamp(lerp(offset.y, target_y, delta*lerp_speed), -vertical_extent, 0)
 
 #	global_position = get_parent().global_position.floor()
 	
